17436db0cdecf1af78e528d3bed9b0902d2d6885
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-06-13 12:11:32 -0500

    prueba_vercel_5

--- app.js
+++ app.js
@@ -38,14 +38,14 @@ app.use("/restricted-service", (req, res, next) => {
 });
 
 app.use("/auth", createProxyMiddleware({ target: "https://user-validation-microservice.vercel.app", changeOrigin: true }));
-//app.use("/q", createProxyMiddleware({ target: "https://queue-microservice.vercel.app", changeOrigin: true }));
+app.use("/q", createProxyMiddleware({ target: "https://queue-microservice.vercel.app", changeOrigin: true }));
 //app.use("/com", createProxyMiddleware({ target: "https://auth-service.vercel.app", changeOrigin: true }));
-//app.use("/game", createProxyMiddleware({ target: "https://match-microservice.vercel.app", changeOrigin: true }));
+app.use("/game", createProxyMiddleware({ target: "https://match-microservice.vercel.app", changeOrigin: true }));
 app.use("/rkn", createProxyMiddleware({ target: "https://ranking-microservice.vercel.app", changeOrigin: true }));
 
 
-app.use("/q", createProxyMiddleware({ target: "http://bmbr.ddns.net:5103", changeOrigin: true }));
+//app.use("/q", createProxyMiddleware({ target: "http://bmbr.ddns.net:5103", changeOrigin: true }));
 //app.use("/com", createProxyMiddleware({ target: "https://auth-service.vercel.app", changeOrigin: true }));
-app.use("/game", createProxyMiddleware({ target: "http://bmbr.ddns.net:5104", changeOrigin: true }));
+//app.use("/game", createProxyMiddleware({ target: "http://bmbr.ddns.net:5104", changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`API Gateway running on port ${PORT}`));
